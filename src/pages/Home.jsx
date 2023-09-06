0573c03861cf2dd8e3c9ef5d233e120e1b7ccb5d
samuelmoiwa <samuelmoiwa@gmail.com>
2023-09-06 13:57:29 +0000

    update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-screen h-96 flex justify-center items-center'>
+    <div className='w-screen h-full flex justify-center items-center flex-col'>
       <div className='bg-green-100 w-10/12 py-14 flex justify-center
-      items-center justify-self-center rounded-md'>
+      items-center justify-self-center rounded-md mt-52'>
       <h1 className='font-bold text-lg text-teal-500'>
-        Home
+        Home 1
+      </h1>
+    </div>
+
+    <div className='bg-green-100 w-10/12 py-14 flex justify-center
+      items-center justify-self-center rounded-md mt-52 mb-10'>
+      <h1 className='font-bold text-lg text-teal-500'>
+        Home 2
       </h1>
     </div>
     </div>
