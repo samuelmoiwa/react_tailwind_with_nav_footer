e909e9e45149cc64fdf321de5370bb477dbadc1a
samuelmoiwa <samuelmoiwa@gmail.com>
2023-09-06 13:53:01 +0000

    add footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
 
-          {/* <Footer /> */}
+          <Footer />
       </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
+    <div className='w-screen h-96 flex justify-center items-center'>
       <div className='bg-green-100 w-10/12 py-14 flex justify-center
       items-center justify-self-center rounded-md'>
       <h1 className='font-bold text-lg text-teal-500'>
