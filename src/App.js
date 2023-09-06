58248d9aa92a67780bb19a5e8dc1ebffd03d86b8
samuelmoiwa <samuelmoiwa@gmail.com>
2023-09-06 13:19:34 +0000

    update add navBar.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
       <>
-          {/* <NavBar /> */}
+          <NavBar />
 
         <Routes>
           <Route path="/" element={<Home />} />
